7f070dee1997051ffb67e8a9541c14a4fa795bf4
Joel <raizetvision@gmail.com>
2025-03-12 13:01:22 +0100

    init

--- games/models.py
+++ games/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class GameCategory(models.Model):
+    code = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
