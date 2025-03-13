27bc6817657557a799b9b8f9ba49a0a3adec366a
Joel <raizetvision@gmail.com>
2025-03-13 08:33:19 +0100

    init

--- users/models.py
+++ users/models.py
@@ -50,7 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
